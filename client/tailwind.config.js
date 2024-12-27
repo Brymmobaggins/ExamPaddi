baac7ae19fd66dfbfedf499552fe724705b4e672
brymmobaggins <ibro2tablet21@yahoo.com>
2024-12-27 18:04:40 +0100

    apply FCC bacjground Color

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -10,6 +10,15 @@ module.exports = {
     "./src/components/**/*.{html,js}",
   ],
   theme: {
+    colors: {
+      "black-haze": {
+        50: "#f5f6f7",
+        100: "#edeff1",
+        200: "#d8dcdf",
+        300: "#b5bec4",
+        400: "#8d9aa3",
+      },
+    },
     extend: {
       fontFamily: {
         sans: "Open Sans, monospace",
